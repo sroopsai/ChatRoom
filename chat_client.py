319da880054b5a2efee0fb7edd60987137d7dcb7
roopsai <roopsai.work@gmail.com>
2019-01-11 08:53:21 +0100

    errors fixed

--- chat_client.py
+++ chat_client.py
@@ -6,7 +6,8 @@ import sys
 import re
 server = socket.socket()
 if len(sys.argv) != 3:
-    print "Correct usage: script, IP address, port number, NICKNAME"
+    print ("Correct usage: For windows terminals --> py chatclient.py serveripaddr:portnum nickname")
+    print ("For Linux terminals --> python chatclient.py serveripaddr:portnum nickname")
     sys.exit(1)
 args = str(sys.argv[1]).split(':')
 host = str(args[0])
@@ -14,31 +15,11 @@ port = int(args[1])
 nick = str(sys.argv[2])
 server.connect((host, port))
 hello = server.recv(1024).decode('ascii')
-server.sendall('NICK '+nick.encode('ascii'))
+server.sendall(('NICK '+nick).encode('ascii'))
 ok = server.recv(1024).decode('ascii')
-
-#while not re.search(r'Welcome to chat room\s\S*',ok) or re.search(r'OK\n',ok):
-#	print(ok)
-#	sys.stdout.write('Enter your name:');
-'''	nick = sys.stdin.readline()
-while not re.search(r'Welcome to chat roo):
-	print(ok)
-	sys.stdout.write('Enter your name:');
-	nick = sys.stdin.readline()
-
-	try:
-		server.sendall('NICK '+nick[:-1].encode('ascii'))
-		ok = server.recv(1024).decode('ascii')
-	except:
-		continue
-'''
-
 if re.search('Error',ok) or re.search('ERROR',ok):
     print(ok)
     sys.exit()
-
-
-
 print(ok)
 while True:
     sockets = [sys.stdin, server]
@@ -47,19 +28,17 @@ while True:
         if sock == server:
             message = sock.recv(2048).decode('ascii')
             if re.search(r'Error',message) or re.search(r'ERROR',message):
-                print message
+                print (message)
             else:
                 message = message[4:]#stripping the message removing MSG and giving it to print out as per the protocol
-                print message
+                print (message)
         else:
             message = sys.stdin.readline()
             if message == '\n':
                 continue
             else:
                 server.sendall('MSG '+message.encode('ascii'))
-
 server.close()
 
             
             
-
